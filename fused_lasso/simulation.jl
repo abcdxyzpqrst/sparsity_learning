ee9259f70f158e8b084cc2ce0f6265fc6efa6587
abcdxyzpqrst <arcprime719@gmail.com>
2019-03-28 10:12:06 +0900

    Fused Lasso with Smooth Proximal Gradient Descent

--- fused_lasso/simulation.jl
+++ fused_lasso/simulation.jl
@@ -1,3 +1,6 @@
+using LinearAlgebra
+using Random
+
 ##### main scripts #####
 include("solvers.jl")
 using Distributions
@@ -7,11 +10,39 @@ using PyCall
 plt = pyimport("matplotlib.pyplot")
 
 ##### control panel #####
-n = 20
+Random.seed!(12345678)
+n = 500
 p = 100 
 σ = 0.75 
-s₁ = 35.6 
-s₂ = Inf
+λ = 0.1/n
+γ = 1.0/n
 
+##### data generation & true parameter with 3 blocks #####
 X = randn(n, p)
-# three consecutive blocks in the true parameter
+θᵀ = zeros(p)
+θᵀ[16:20] = randn() * ones(5)
+θᵀ[51:60] = randn() * ones(10)
+θᵀ[90:96] = randn() * ones(7)
+y = X*θᵀ + σ*randn(n)
+
+#### learning parameter & edge-vertex matrix #####
+θ = randn(p)
+C = zeros(p-1, p)
+
+for j = 2:p
+    C[j-1, j-1] = γ
+    C[j-1, j] = -γ
+end
+
+solve_fused_lasso(X, y, θ, λ, γ, C, θᵀ)
+
+x = collect(1:1:p)
+plt.rc("text", usetex=true)
+plt.rc("font", family="Times New Roman", size=12)
+plt.figure()
+plt.xlabel("Predictor")
+plt.ylabel("Coefficient")
+plt.plot(x, θᵀ, label="True")
+plt.plot(x, θ, label="Estimation")
+plt.legend(loc="best")
+plt.savefig("simulation.png", bbox_inches="tight", transparent=true, dpi=600)
--- fused_lasso/solvers.jl
+++ fused_lasso/solvers.jl
@@ -1,12 +1,67 @@
 using LinearAlgebra
+using Distributions
 
-# TODO: FLSA, Smooth-Prox Comparison
-function objective(X, y, θ, λ, μ)
+function objective(X, y, θ, λ, γ)
     s = 0.0                         # fusion penalty 
     for j = 2:length(θ)
         s += abs(θ[j] - θ[j-1])
     end
-    return 0.5*sum(abs2, X*θ - y) + λ*sum(abs, θ) + μ*s
+    return 0.5*mean(abs2, X*θ - y) + λ*sum(abs, θ) + γ*s
 end
 
+function proj_max_ball(θ)
+    for i in eachindex(θ)
+        θ[i] >  1 ? θ[i] =  1.0 :
+        θ[i] < -1 ? θ[i] = -1.0 :
+        nothing;
+    end
+    return θ
+end
 
+function prox_ℓ₁_norm(θ, κ)
+    for i in eachindex(θ)
+         abs(θ[i]) > κ ? θ[i] = sign(θ[i])*max(abs(θ[i]) - κ, 0) :
+         θ[i] = 0.0
+     end
+end
+
+function solve_fused_lasso(X, y, θ, λ, γ, C, θᵀ; itm=1000, tol=1e-6, ptf=100)
+    # fused lasso with smoothing proximal gradient descent
+    θ⁻ = copy(θ)
+    n = size(X, 1)
+    p = length(θ)
+    ϵ = 1e-8
+    D = (p-1)/2
+    μ = ϵ/(2*D)
+    L = opnorm(X)^2 + (2*γ)^2 / μ
+    η = 1/L
+    κ = η*λ
+
+    noi = 0
+    
+    true_obj = objective(X, y, θᵀ, λ, γ)
+    obj = objective(X, y, θ, λ, γ)
+
+    err = obj - true_obj
+    while err ≥ ϵ
+        # gradient step on θ
+        α = proj_max_ball(C*θ ./ μ)
+        Δθ = X'*(X*θ - y)/n + C'*α
+        BLAS.axpy!(-η, Δθ, θ)
+        
+        # proximal operation
+        prox_ℓ₁_norm(θ, κ)
+        
+        # learning information
+        obj = objective(X, y, θ, λ, γ)
+        #err = norm(θ⁻ - θ)/η
+        err = obj - true_obj
+        copyto!(θ⁻, θ)
+
+        noi += 1
+        if noi % ptf == 0 
+            println("err: ", err, "\nobj: ", obj, "\n")
+        end
+    end
+    return θ
+end
