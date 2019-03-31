455b92ab61917689158dd811fe96b40b7a59d34e
abcdxyzpqrst <arcprime719@gmail.com>
2019-03-31 14:47:26 +0900

    Solvers for non-convex penalties

--- nonconvex_penalties/solvers.jl
+++ nonconvex_penalties/solvers.jl
@@ -6,13 +6,9 @@ Parameter Configurations
 X:  observations
 y:  responses
 θ:  variables
-w:  weight-parameter for θ
 λ:  coefficient of regularizer
 ρ:  parameter for nonconvex penalties, SCAD and MCP
 κ:  proximal parameters (κ = η*λ)
-τ:  proximal parameter for w
-α:  belief of sparsity level
-h:  trimming outliers
 """
 
 # Just utils
@@ -25,10 +21,6 @@ function eye(p)
 end
 
 # Since the objective is "sum", the corresponding λ value should be scaled
-function trim_objective(X, y, θ, w, λ)
-    return 0.5*sum(abs2, X*θ - y) + λ*sum(abs, θ.*w)
-end
-
 function scad_objective(X, y, θ, ρ, λ)
     return 0.5*sum(abs2, X*θ - y) + λ*scad(θ, λ, ρ)
 end
@@ -74,41 +66,6 @@ function prox_ℓ₁_norm(θ, κ)
     end
 end
 
-function prox_trimmed_ℓ₁_norm(θ, w, κ)
-    for i in eachindex(θ)
-        ν = κ*w[i]
-        abs(θ[i]) > ν ? θ[i] = sign(θ[i])*max(abs(θ[i])-ν, 0) :
-        θ[i] = 0.0
-    end
-end
-
-function prox_ℓ₁₂_norm(θ, w, κ)
-    for i = 1:length(w)
-        ν = κ*w[i]
-        ρ = vecnorm(x[i, :]); r = ρ;
-        ρ == 0.0 && continue;
-        ρ > ν ? r -= ν : r = 0.0;
-        x[i, :] = (r/ρ) * x[i, :]
-    end
-end
-
-function prox_capped_simplex(w, lb, ub, h)
-    h == length(w) && (fill!(w, 1.0); return nothing;)
-    h  > length(w) && error("wrong h or size of vector!");
-
-    h = Float64(h)
-    f(α) = sum(max.(min.(w .- α, ub), lb)) - h
-    a = -1.5 + minimum(w)
-    b = maximum(w)
-    α = fzero(f, [a, b])
-    for i in eachindex(w)
-        w[i] -= α
-        w[i] > ub ? w[i] = ub :
-        w[i] < lb ? w[i] = lb :
-        nothing
-    end
-end
-
 function prox_scad(θ, ρ, κ)
     for i in eachindex(θ)
         abs(θ[i]) < 2*κ ? θ[i] = sign(θ[i])*max(abs(θ[i])-κ, 0) :
@@ -122,39 +79,6 @@ function prox_mcp(θ, ρ, κ)
         abs(θ[i]) > ρ*κ ? θ[i] = θ[i] :
         abs(θ[i]) ≤ κ ? θ[i] = 0.0 :
         θ[i] = (ρ*θ[i] - sign(θ[i])*ρ*κ)/(ρ-1)
-    end
-end
-
-# From here, we use vanilla gradient descent, ∇θ = ∂𝓛/∂θ, θ⁽ᵗ⁺¹⁾ = θ⁽ᵗ⁾ - η∇θ
-function solve_trim_lasso(X, y, θ, w, h, λ; itm=1000, tol=1e-6, ptf=100)
-    θ⁻ = copy(θ)
-    η  = 1.0/opnorm(X)^2
-    κ  = η*λ
-    τ  = 1.0
-
-    err = 1.0
-    noi = 0
-
-    while err ≥ tol
-        # proximal gradient on θ
-        ∇θ = X'*(X*θ - y)
-        BLAS.axpy!(-η, ∇θ, θ)
-        prox_trimmed_ℓ₁_norm(θ, w, κ)
-
-        # projected gradient on w
-        BLAS.axpy!(-τ, abs.(θ), w)
-        prox_capped_simplex(w, 0.0, 1.0, h)
-        
-        # update informations
-        obj = trim_objective(X, y, θ, w, λ)
-        err = norm(θ⁻ - θ)/η
-        copyto!(θ⁻, θ)
-
-        # print learning information
-        noi += 1
-        #noi % ptf == 0 && @printf("iter %4d, obj %1.2e, err %1.2e\n",
-        #                          noi, obj, err);
-        noi ≥ itm && break;
     end
 end
 
